93ee6744a7d82bd5902fa5226bd8b821f2e70621
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-04-20 00:39:29 +0300

    add styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import { Button } from '@material-ui/core';
+import { Box, Button, Paper } from '@material-ui/core';
 import { interval, Subject } from 'rxjs';
-import { take, takeUntil } from 'rxjs/operators';
+import { takeUntil } from 'rxjs/operators';
+import image from './images/istockphoto-978715528-612x612.jpg';
 
 function App() {
   const [time, setTime] = useState(0);
   const [watchOn, setWatchOn] = useState(false);
 
+  const numbers = interval(10);
+
   useEffect(() => {
     const unsubscribe = new Subject();
 
-    interval(10)
+    numbers
       .pipe(takeUntil(unsubscribe))
       .subscribe((value) => {
         if (watchOn) {
@@ -20,7 +23,6 @@ function App() {
       });
     return () => {
       unsubscribe.next();
-      unsubscribe.complete();
     };
   }, [watchOn]);
 
@@ -43,43 +45,77 @@ function App() {
   }
 
   return (
-    <>
-      <div>
-        <span>{('0' + Math.floor((time / (1000 * 60 * 60)) % 24)).slice(-2)}</span>&nbsp;:&nbsp;
-        <span>{('0' + Math.floor(time / 6000)).slice(-2)}</span>&nbsp;:&nbsp;
-        <span>{('0' + Math.floor((time / 100) % 60)).slice(-2)}</span>&nbsp;:&nbsp;
-        <span>{('0' + Math.floor(time % 100)).slice(-2)}</span>
-      </div>
+    <Paper style={styles.paperContainer}>
+      <Box
+        display="flex"
+        height="100vh"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Box>
+          <Box display="flex" justifyContent="center" m={2}>
+            <Box fontSize="2.4rem">
+              {('0' + Math.floor((time / (1000 * 60 * 60)) % 24)).slice(-2)}&nbsp;:&nbsp;
+            </Box>
+            <Box fontSize="2.4rem">
+              {('0' + Math.floor(time / 6000)).slice(-2)}&nbsp;:&nbsp;
+            </Box>
+            <Box fontSize="2.4rem">
+              {('0' + Math.floor((time / 100) % 60)).slice(-2)}&nbsp;:&nbsp;
+            </Box>
+            <Box fontSize="2.4rem">
+              {('0' + Math.floor(time % 100)).slice(-2)}
+            </Box>
+          </Box>
 
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={startTimer}
-      >
-        Start
-      </Button>
-      <Button
-        variant="contained"
-        color="secondary"
-        onClick={stopTimer}
-      >
-        Stop
-      </Button>
-      <Button
-        variant="contained"
-        onClick={pauseTimer}
-      >
-        Wait
-      </Button>
-      <Button
-        variant="outlined"
-        color="secondary"
-        onClick={resetTimer}
-      >
-        Reset
-      </Button>
-    </>
+          <Box display="flex" justifyContent="center">
+            <Box m={2}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={startTimer}
+              >
+                Start
+              </Button>
+            </Box>
+            <Box m={2}>
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={stopTimer}
+              >
+                Stop
+              </Button>
+            </Box>
+            <Box m={2}>
+              <Button
+                variant="contained"
+                onDoubleClick={pauseTimer}
+              >
+                Wait
+              </Button>
+            </Box>
+            <Box m={2}>
+              <Button
+                variant="outlined"
+                color="secondary"
+                onClick={resetTimer}
+              >
+                Reset
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Paper>
   );
 }
+
+const styles = {
+  paperContainer: {
+    backgroundImage: `url(${image})`,
+    backgroundSize: 'cover',
+  }
+};
 
 export default App;
